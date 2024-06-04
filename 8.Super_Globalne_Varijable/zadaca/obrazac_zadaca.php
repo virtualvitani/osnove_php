50ecc4848f9f968be14c1bf7586e9f2364ab2cc0
virtualvitani <lukas.premuzi@gmail.com>
2024-06-04 18:02:30 +0200

    Dodana zadaca za 8

---
+++ 8.Super_Globalne_Varijable/zadaca/obrazac_zadaca.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form method="POST" action="obrada_zadaca.php">
+        <label for="first_name">Ime</label>
+        <input type="text" id="first_name" name="first_name" ><br><br>
+        <label for="last_name">Prezime</label>
+        <input type="text" id="last_name" name="last_name" ><br><br>
+        <input type="submit" value="Posalji">
+    </form>
+
+</body>
+
+</html>
